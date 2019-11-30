d996fa94422c61b79b127a16c35dcd784451ba7c
No-Max <malyavko.max@gmail.com>
2019-11-30 16:08:40 +0300

    BE - add categories endpoints

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 const worksController = require('./../controllers/works.js');
 const detailsController = require('./../controllers/details.js');
+const categoriesController = require('./../controllers/categories.js');
 
 exports.init = function(app) {
     // works
@@ -15,4 +16,11 @@ exports.init = function(app) {
     app.post('/details', detailsController.create);
     app.put('/details/:id', detailsController.update);
     app.delete('/details/:id', detailsController.delete);
+
+    // categories
+    app.get('/categories', categoriesController.all);
+    app.get('/categories/:id', categoriesController.findById);
+    app.post('/categories', categoriesController.create);
+    app.put('/categories/:id', categoriesController.update);
+    app.delete('/categories/:id', categoriesController.delete);
 };
