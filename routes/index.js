5d8220789aa2329bf70090b8859f83c2807aeb03
Max Maliauka <m.maliauka@godeltech.com>
2020-01-23 16:43:40 +0300

    fix removed routes file issue

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const apiRoutes = require('./api-routes.js');
-const clientRoutes = require('./client-routes.js');
 
 exports.init = function(app) {
-    clientRoutes.init(app);
     apiRoutes.init(app);
 };
